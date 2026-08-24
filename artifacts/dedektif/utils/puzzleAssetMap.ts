4d057f76f2e23153b0c242d38fc5f689ac467738
5N1Dedektif <5n1katildedektiflikoyunu@gmail.com>
2026-08-24 12:39:44 +0300

    chore(avatars): register  assets

--- artifacts/dedektif/utils/puzzleAssetMap.ts
+++ artifacts/dedektif/utils/puzzleAssetMap.ts
@@ -626,6 +626,15 @@ const PUZZLE_ASSET_MAP: Record<string, number> = {
   sf_002_l1: require("../assets/images/puzzle_assets/sf_002_l1.png"),
   sf_002_l2: require("../assets/images/puzzle_assets/sf_002_l2.png"),
   sf_002_l3: require("../assets/images/puzzle_assets/sf_002_l3.png"),
+  sf_003_s1: require("../assets/images/puzzle_assets/sf_003_s1.png"),
+  sf_003_s2: require("../assets/images/puzzle_assets/sf_003_s2.png"),
+  sf_003_s3: require("../assets/images/puzzle_assets/sf_003_s3.png"),
+  sf_003_w1: require("../assets/images/puzzle_assets/sf_003_w1.png"),
+  sf_003_w2: require("../assets/images/puzzle_assets/sf_003_w2.png"),
+  sf_003_w3: require("../assets/images/puzzle_assets/sf_003_w3.png"),
+  sf_003_l1: require("../assets/images/puzzle_assets/sf_003_l1.png"),
+  sf_003_l2: require("../assets/images/puzzle_assets/sf_003_l2.png"),
+  sf_003_l3: require("../assets/images/puzzle_assets/sf_003_l3.png"),
 };
 
 export function isPuzzleAsset(icon: string | undefined): boolean {
